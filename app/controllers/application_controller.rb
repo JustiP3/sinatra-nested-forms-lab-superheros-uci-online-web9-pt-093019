59d2e8c9b03cac218377ac3ae0ee24f3d7ebacd5
Justin Phillips <justinphil02@yahoo.com>
2020-01-25 22:20:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,22 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      @params = params
-
+      @team = Team.new(params[:team][:name],params[:team][:motto])
+      @heroes = params[:team][:members].map do |member|
+        name = member[:name]
+        power = [:power]
+        bio = [:bio]
+        Hero.new(name, power, bio)
+      end
       erb :team
     end
 
-
+    <h1><%= @params[:team][:name] %></h1>
+    <h3><%= @params[:team][:motto] %></h3>
+    <% @params[:team][:members].each do |member| %>
+    <h2>Hero Name: <%= member[:name] %></h2>
+    <p>Hero Name: <%= member[:power] %></p>
+    <p>Hero Biography: <= member[:bio]</p>
+    <% end %>
 
 end
