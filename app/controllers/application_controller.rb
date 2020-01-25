12237cfd36595c3e7c1af88cdb5df099a711c706
Justin Phillips <justinphil02@yahoo.com>
2020-01-25 22:45:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
         name = member[:name]
         power = member[:power]
         bio = member[:bio]
-        binding.pry
+      #  binding.pry
         Hero.new(name, power, bio)
       end
 
